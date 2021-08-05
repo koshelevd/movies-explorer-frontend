cdcabf4ecd4b4af2ad7796dc173efbd63c5325e5
Dmitrii Koshelev <me@koshelev.net>
2021-08-05 12:29:21 +0300

    Refactor NavTab component

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import './NavTab.css';
 
 function NavTab({ scroller, refs }) {
-  console.log(refs);
   return (
     <nav className="nav-tab">
       <ul className="nav-tab__list">
