0ab73caade94d32b4a50aeff4e30a7d0c18ecd53
Dmitrii Koshelev <me@koshelev.net>
2021-08-05 13:13:24 +0300

    Fix links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import './Portfolio.css';
 
 function Portfolio() {
@@ -7,24 +6,39 @@ function Portfolio() {
       <h2 className="portfolio__header">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__list-item">
-          <Link to="/" className="portfolio__link link smoothly">
+          <a
+            href="https://koshelevd.github.io/how-to-learn/"
+            className="portfolio__link link smoothly"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="portfolio__link-title">Статичный сайт</span>
             <span className="portfolio__link-icon">&#8599;</span>
-          </Link>
+          </a>
         </li>
         <li className="portfolio__list-item">
-          <Link to="/" className="portfolio__link link smoothly">
+          <a
+            href="https://koshelevd.github.io/russian-travel/"
+            className="portfolio__link link smoothly"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="portfolio__link-title">Адаптивный сайт</span>
             <span className="portfolio__link-icon">&#8599;</span>
-          </Link>
+          </a>
         </li>
         <li className="portfolio__list-item">
-          <Link to="/" className="portfolio__link link smoothly">
+        <a
+            href="https://mesto.koshelev.net/"
+            className="portfolio__link link smoothly"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="portfolio__link-title">
               Одностраничное приложение
             </span>
             <span className="portfolio__link-icon">&#8599;</span>
-          </Link>
+          </a>
         </li>
       </ul>
     </section>
