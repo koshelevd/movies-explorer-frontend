95725cbeb1fcf20b560af0286aa5407a5bb827a5
Dmitrii Koshelev <me@koshelev.net>
2021-08-05 12:26:29 +0300

    Refactor SearchForm component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,22 +1,55 @@
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
+import {
+  ACTIVE_ERROR_SELECTOR,
+  SEARCH_ERROR_MESSAGE,
+} from '../../utils/config';
 
-function SearchForm() {
+function SearchForm({
+  onSearch,
+  searchInputRef,
+  onFilterChange,
+  isChecked,
+  searchFormIsValid,
+  isLoading,
+}) {
+  function searchMovies(e) {
+    e.preventDefault();
+    onSearch();
+  }
+
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={searchMovies}>
       <div className="search-form__container">
         <input
-          type="text"
+          type="search"
           className="search-form__input smoothly"
           placeholder="Фильм"
+          name="search"
+          autoComplete="off"
+          autoFocus
+          ref={searchInputRef}
+          disabled={isLoading}
         />
         <input
           type="submit"
           className="search-form__submit smoothly"
           value=""
+          disabled={isLoading}
         />
       </div>
-      <FilterCheckbox label="Короткометражки" checked={true} />
+      <span
+        className={`search-form__validation-error form__validation-error ${
+          searchFormIsValid ? '' : ACTIVE_ERROR_SELECTOR
+        }`}
+      >
+        {SEARCH_ERROR_MESSAGE}
+      </span>
+      <FilterCheckbox
+        label="Короткометражки"
+        checked={isChecked}
+        onChange={onFilterChange}
+      />
     </form>
   );
 }
