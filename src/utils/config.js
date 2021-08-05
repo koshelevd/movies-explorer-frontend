9f2237e974ccc62605b28873bd9f418d99207a12
Dmitrii Koshelev <me@koshelev.net>
2021-08-05 12:21:00 +0300

    Store project constant in config file

---
+++ src/utils/config.js
@@ -0,0 +1,62 @@
+export const MOVIES_API_URL = 'https://api.nomoreparties.co';
+export const MOVIES_API_ENDPOINT = '/beatfilm-movies';
+export const MAIN_API_URL = 'https://api.movies.koshelev.net';
+export const SIGN_UP_ENDPOINT = '/signup';
+export const SIGN_IN_ENDPOINT = '/signin';
+export const SIGN_OUT_ENDPOINT = '/signout';
+export const PROFILE_ENDPOINT = '/users/me';
+export const MOVIES_ENDPOINT = '/movies';
+export const ACTIVE_ERROR_SELECTOR = 'form__validation-error_active';
+export const INPUT_ERROR_SELECTOR = 'form__input_type_error';
+export const MOBILE_WIDTH = 480;
+export const TABLET_WIDTH = 768;
+export const DESKTOP_WIDTH = 1280;
+export const CARDS_TO_SHOW_MOBILE = 5;
+export const CARDS_TO_SHOW_TABLET = 8;
+export const CARDS_TO_SHOW_DESKTOP = 12;
+export const MORE_CARDS_MOBILE = 1;
+export const MORE_CARDS_TABLET = 2;
+export const MORE_CARDS_DESKTOP = 3;
+export const ERROR_MESSAGE_TIMEOUT = 3000;
+export const SIGN_UP_ERROR_DEFAULT_MESSAGE =
+  'При регистрации пользователя произошла ошибка.';
+export const SIGN_IN_ERROR_DEFAULT_MESSAGE =
+  'При авторизации произошла ошибка.';
+export const SIGN_OUT_ERROR_DEFAULT_MESSAGE = 'Произошла ошибка.';
+export const USER_UPDATE_ERROR_DEFAULT_MESSAGE =
+  'При обновлении профиля произошла ошибка.';
+export const BAD_REQUEST_ERROR_CODE = 400;
+export const UNAUTHORIZED_ERROR_CODE = 401;
+export const NOT_FOUND_ERROR_CODE = 404;
+export const CONFLICT_ERROR_CODE = 409;
+export const INTERNAL_SERVER_ERROR_CODE = 500;
+export const BAD_REQUEST_MESSAGE =
+  'Ошибка в запросе. Проверьте введенные данные';
+export const UNAUTHORIZED_ERROR_MESSAGE =
+  'Вы ввели неправильный логин или пароль.';
+export const NOT_FOUND_ERROR_MESSAGE =
+  '404 Страница по указанному маршруту не найдена.';
+export const CONFLICT_ERROR_MESSAGE =
+  'Пользователь с таким email уже существует.';
+export const INTERNAL_SERVER_ERROR_MESSAGE = '500 На сервере произошла ошибка';
+export const ERROR_TEXT = 'Ошибка';
+export const DEFAULT_EMPTY_VALUE = '-пусто-';
+export const MOVIES_PAGE_TITLE = 'Фильмы - Movie explor';
+export const SAVED_MOVIES_PAGE_TITLE = 'Сохранённые фильмы - Movie explor';
+export const PROFILE_PAGE_TITLE = 'Профиль - Movie explor';
+export const REGISTER_PAGE_TITLE = 'Регистрация - Movie explor';
+export const LOGIN_PAGE_TITLE = 'Войти - Movie explor';
+export const BUTTON_TYPE_DELETE = 'button_type_delete';
+export const BUTTON_TYPE_SAVED = 'button_type_saved';
+export const BUTTON_TYPE_SAVE = 'button_type_save';
+export const SAVE_BUTTON_TEXT = 'Сохранить';
+export const MORE_BUTTON_TEXT = 'Ещё';
+export const NOT_FOUND_TEXT = 'Ничего не найдено';
+export const SHORT_FILM_DURATION = 40;
+export const PAGES_WITH_HEADER = ['/', '/movies', '/saved-movies', '/profile'];
+export const PAGES_WITH_FOOTER = ['/', '/movies', '/saved-movies'];
+export const SEARCH_ERROR_MESSAGE = 'Нужно ввести ключевое слово';
+export const SEARCH_RESULT_ERROR =
+  'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз';
+export const PROFILE_SAVE_SUCCESS_MESSAGE =
+  'Данные пользователя успешно обновлены.';
